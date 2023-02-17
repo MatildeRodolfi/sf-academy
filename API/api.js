16fc7db458a1d4f82815a1cec2b657918703a8b9
MatildeRodolfi <rodolfi.matilde@gmail.com>
2023-02-17 10:28:26 +0100

    login & user management session - users microservice

--- API/api.js
+++ API/api.js
@@ -4,6 +4,14 @@ const { join } = require("path")
 const descriptor = grpc.loadPackageDefinition(protoLoader.loadSync(join(__dirname, "../proto/users.proto")))
 const grpcClient = new descriptor.greeter.Greeter("0.0.0.0:9001", grpc.credentials.createInsecure())
 
-grpcClient.signup({email: "bho@nonso.it", password: "password", name: "MarioRossi", iban: "IT0000"}, (err, data) => {
+/*grpcClient.signup({email: "bho@nonso.it", password: "password", name: "MarioRossi", iban: "IT0000"}, (err, data) => {
   console.log({ err, data })
 })
+
+grpcClient.login({email: "bho@nonso.it", password: "password"}, (err, data) => {
+    console.log({ err, data })
+})
+
+grpcClient.refreshToken({email: "bho@nonso.it", token: ""}, (err, data) => {
+    console.log({ err, data })
+})*/
--- users/users.js
+++ users/users.js
@@ -10,21 +10,23 @@ const jwtExpirySeconds = 300
 
 
 
+/** HASHING PASSWORD - cripta la pw e restituisce hash e salt*/
 function hashingPassword(password) { 
-    
     salt = crypto.randomBytes(16).toString('hex'); 
     pw = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`); 
 
     return {salt, pw}
 }; 
 
-function validPassword (password) { 
-    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`); 
-    return this.hash === hash; 
+/** VALID PASSWORD - verifica che la pw inserita sia corretta*/
+function validPassword (password, hash, salt) { 
+    var newhash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`); 
+    return newhash === hash; 
 }; 
 
 
 
+/** dati di accesso alla db */
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'my_user',
@@ -34,7 +36,8 @@ const pool = new Pool({
   port: 5432,
 });
 
-function saveUserDb(email, name, iban, salt, pw){
+/** SAVE USER - salvataggio dati utente su db*/
+function saveUser(email, name, iban, salt, pw){
     return new Promise(function(resolve, reject) {
 
         var query = 'INSERT INTO users (email, name, iban, password, salt) VALUES ('
@@ -56,8 +59,27 @@ function saveUserDb(email, name, iban, salt, pw){
     })
 }
 
+/** GET PASSWORD AND SALT OF USER - restituisce hash e salt dell'utente dal db*/
+function getPasswordAndSaltOfUser(email){
+    return new Promise(function(resolve, reject) {
+
+        var query = 'SELECT password, salt FROM users WHERE email=\''+email+'\''; 
+        pool.query(query, (error, results) => {
+            if (error) {
+                reject(error);
+                console.log(error);
+            }
+            else{
+                console.log('get user\'s hash and salt from db');
+            }
+            resolve(results.rows);
+        })
+    })
+}
 
 
+
+/** CREATE TOKEN - crea il il token JWT partendo dalla mail dell'utente*/
 function createToken(mail){
     const token = jwt.sign({mail}, jwtKey, {
 		algorithm: "HS256",
@@ -69,6 +91,7 @@ function createToken(mail){
 
 
 
+/** GET EXCHANGE - restituisce il valore di cambio comunicando con il server grpc exchange */
 function getExchange(value, from, to){
     const descriptorExchange = grpc.loadPackageDefinition(protoLoader.loadSync(join(__dirname, "../proto/exchange.proto")))
     const grpcClient = new descriptorExchange.greeter.Greeter("0.0.0.0:9000", grpc.credentials.createInsecure())
@@ -86,10 +109,12 @@ function getExchange(value, from, to){
 
 
 const implementations = {
+
+    /** SIGNUP -  salva l'utente sul db e restituisce il token JWT*/
     signup: (call, callback) => {
 
         if (!call.request.email || !call.request.password || !call.request.name || !call.request.iban){
-            console.log("invalid input");
+            console.log("signup - invalid input");
             return callback({
                 code: 400,
                 message: "one or more empty input",
@@ -99,7 +124,7 @@ const implementations = {
 
         var {salt, pw} = hashingPassword(call.request.password);
         
-        saveUserDb(call.request.email, call.request.name, call.request.iban, salt, pw)
+        saveUser(call.request.email, call.request.name, call.request.iban, salt, pw)
         .then(response => {
             var {token, maxAge}= createToken(call.request.email);
 
@@ -110,6 +135,7 @@ const implementations = {
         })
         .catch(error => {
             if (error.code==23505){
+                console.log("signup - duplicate email");
                 return callback({
                     code: 409,
                     message: "duplicate record: user with this email already exists",
@@ -117,6 +143,7 @@ const implementations = {
                 });
             }
             else{
+                console.log("signup - internal error with db");
                 return callback({
                     code: 500,
                     message: "internal error with insert into db",
@@ -125,9 +152,114 @@ const implementations = {
             }
             
         })   
+    },
+
+    /** LOGIN -  controlla dati utente e restituisce il token JWT*/
+    login: (call, callback) => {
+
+        if (!call.request.email || !call.request.password){
+            console.log("login - invalid input");
+            return callback({
+                code: 400,
+                message: "one or more empty input",
+                status: grpc.status.INTERNAL
+            });
+        }
+        
+        getPasswordAndSaltOfUser(call.request.email)
+        .then(response => {
+            
+            var pw = response[0].password;
+            var salt = response[0].salt;
+
+            if (validPassword(call.request.password, pw, salt)){
+
+                console.log("user authenticated");
+                var {token, maxAge}= createToken(call.request.email);
+
+                return callback(null, {
+                    token: token,
+                    maxAge: maxAge
+                })
+            }
+            
+            console.log("login - wrong password");
+            return callback({
+                code: 401,
+                message: "wrong password",
+                status: grpc.status.INTERNAL
+            });
+            
+        })
+        .catch(error => {
+            console.log("login - internal error with db");
+            return callback({
+                code: 500,
+                message: "internal error with db",
+                status: grpc.status.INTERNAL
+            });
+        })   
+    },
+
+    /** REFRESH TOKEN -  rigenera il token JWT, controllando se è un token corretto e che sia in scadenza*/
+    refreshToken: (call, callback) =>{
+        if (!call.request.email) {
+            console.log("refreshToken - empty email");
+            return callback({
+                code: 400,
+                message: "empty email",
+                status: grpc.status.INTERNAL
+            });
+        }
+
+        if (!call.request.token) {
+            console.log("refreshToken - empty token");
+            return callback({
+                code: 401,
+                message: "empty token",
+                status: grpc.status.INTERNAL
+            });
+        }
+    
+        var payload
+        try {
+            payload = jwt.verify(call.request.token, jwtKey)
+        } catch (e) {
+            if (e instanceof jwt.JsonWebTokenError) {
+                console.log("refreshToken - wrong token");
+                return callback({
+                    code: 401,
+                    message: "wrong token",
+                    status: grpc.status.INTERNAL
+                });
+            }
+            console.log("refreshToken - internal error with db");
+            return callback({
+                code: 500,
+                message: "internal error with db",
+                status: grpc.status.INTERNAL
+            });
+        }
+        const nowUnixSeconds = Math.round(Number(new Date()) / 1000)
+        if (payload.exp - nowUnixSeconds > 30) {
+            console.log("refreshToken - too early");
+            return callback({
+                code: 400,
+                message: "have to wait more before ask to refresh token",
+                status: grpc.status.INTERNAL
+            });
+        }
+    
+        var {token, maxAge} = createToken(call.request.email);
+        onsole.log("token refreshed");
+        return callback(null, {
+            token: token,
+            maxAge: maxAge
+        })
     }
 }
 
+/** avvio users grpc server */
 const descriptorUsers = grpc.loadPackageDefinition(protoLoader.loadSync(join(__dirname, "../proto/users.proto")))
 const server = new grpc.Server()
 server.bindAsync = promisify(server.bindAsync)
